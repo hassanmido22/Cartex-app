e189eb98414b897a090da6d5410ab010c2bd47c5
omarzero130 <omarzero130@gmail.com>
2020-06-12 11:48:02 +0200

    Revert "last_changes"
    This reverts commit e544745fcdfd70690ed0134d2e2821f52b0f2136.

--- lib/Screens/barcode_test.dart
+++ lib/Screens/barcode_test.dart
@@ -98,7 +98,7 @@ class _BarcodeTestState extends State<BarcodeTest> {
         context,
         MaterialPageRoute(
             builder: (context) =>
-                SingleProduct(id:result.rawContentF)),
+                SingleProduct(id: int.parse(result.rawContent))),
       );
     } on PlatformException catch (e) {
       var result = ScanResult(
--- lib/Screens/singleProduct.dart
+++ lib/Screens/singleProduct.dart
@@ -6,7 +6,7 @@ import 'package:gp_login_screen/Providers/UserProvider.dart';
 import 'package:gp_login_screen/Screens/singleProductwidget.dart';
 
 class SingleProduct extends StatelessWidget {
-  final String id;
+  final int id;
 
   const SingleProduct({Key key, this.id}) : super(key: key);
 
@@ -35,7 +35,7 @@ class SingleProduct extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: Container(
+              child: Container(
             child: FutureBuilder<Product>(
           future: listSelectedProducts(id),
           builder: (context, snapshot) {
@@ -51,66 +51,65 @@ class SingleProduct extends StatelessWidget {
         )),
       ),
       bottomNavigationBar: BottomAppBar(
-        elevation: 1,
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // changes position of shadow
-                ),
+          elevation: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(17)),
+            height: 90,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 6,
+                    child: Container(
+                      height: 50,
+                      margin: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        color: Color.fromRGBO(238, 76, 125, 1),
+                        onPressed: () {},
+                        child: Text(
+                          "Add to cart",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white),
+                        ),
+                      ),
+                    )),
+                Expanded(
+                    flex: 4,
+                    child: Container(
+                      height: 50,
+                      margin: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        color: Color.fromRGBO(110, 167, 216, 1),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Scan",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ))
               ],
-              borderRadius: BorderRadius.circular(17)),
-          height: 90,
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                  flex: 6,
-                  child: Container(
-                    height: 50,
-                    margin: EdgeInsets.all(10),
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25)),
-                      color: Color.fromRGBO(238, 76, 125, 1),
-                      onPressed: () {},
-                      child: Text(
-                        "Add to cart",
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white),
-                      ),
-                    ),
-                  )),
-              Expanded(
-                  flex: 4,
-                  child: Container(
-                    height: 50,
-                    margin: EdgeInsets.all(10),
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25)),
-                      color: Color.fromRGBO(110, 167, 216, 1),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        "Scan",
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white),
-                      ),
-                    ),
-                  ))
-            ],
-          ),
-        ),
-      ),
+            ),
+          )),
     );
   }
 }
